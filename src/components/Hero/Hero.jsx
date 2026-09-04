51e04b95577917b728dbf147025e485866c5648a
Suleyman <amanovsuleyman2000@gmail.com>
2026-09-04 14:40:46 +0500

    container hero

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -2,12 +2,12 @@ import Slider from "./Slider";
 import Container from "../Container/Container";
 
 const Hero = () => {
-   
-
     return (
-        <div className='md:max-w-338 md:w-full md:px-[1.6rem] md:mx-auto'>
-            <Slider />
-        </div>
+        <Container>
+            <div className='md:max-w-338 md:w-full md:px-[1.6rem] md:mx-auto'>
+                <Slider />
+            </div>
+        </Container>
     );
 };
 
