5995d03ea98f6146c92cb0a636b348aa27dc6559
Suleyman <amanovsuleyman2000@gmail.com>
2026-09-14 10:40:18 +0500

    Our Partners Page is done

--- src/pages/ourPartners/OurPartners.jsx
+++ src/pages/ourPartners/OurPartners.jsx
@@ -1,13 +1,71 @@
+import { useTranslation } from "react-i18next";
 import Breadcrumbs from "../../components/Breadcrumbs/Breadcrumbs";
 import Container from "../../components/Container/Container";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import AnyQuestions from "../../components/AnyQuestions/AnyQuestions";
 
 const OurPartners = () => {
-    return (
-        <Container>
-            <Breadcrumbs />
+    const { t } = useTranslation();
 
-            <h1 className='text-5xl'> OurPArtners</h1>
-        </Container>
+    const [expanded, setExpanded] = useState(false);
+
+    const handleMore = (item, element) => {
+        if (item.text.includes("компаний «КАМАЗ»")) {
+            element.classList.add("line-clamp-none");
+        }
+    };
+
+    return (
+        <div>
+            <Container>
+                <Breadcrumbs />
+
+                <h1 className='text-2xl font-medium mb-8'>
+                    {t("partnersPage.title")}
+                </h1>
+
+                <div className='flex flex-col gap-19'>
+                    {t("partnersPage.companies", { returnObjects: true }).map(
+                        (item) => {
+                            return (
+                                <div className='max-w-200'>
+                                    <h1 className='mb-8 text-2xl font-medium'>
+                                        {item.title}
+                                    </h1>
+                                    <p
+                                        className={`${item.text.includes("компаний «КАМАЗ»") && !expanded ? "line-clamp-6" : ""}`}
+                                    >
+                                        {item.text}
+                                    </p>
+
+                                    {item.text.includes("компаний «КАМАЗ»") && (
+                                        <p
+                                            onClick={() =>
+                                                setExpanded((prev) => !prev)
+                                            }
+                                            className='underline mt-1 cursor-pointer opacity-50'
+                                        >
+                                            {expanded
+                                                ? "Свернуть"
+                                                : "Читать полностью"}
+                                        </p>
+                                    )}
+
+                                    <Link
+                                        className='text-gray-400! font-normal underline'
+                                        to={item.link}
+                                    >
+                                        {item.link}
+                                    </Link>
+                                </div>
+                            );
+                        },
+                    )}
+                </div>
+            </Container>
+            <AnyQuestions />
+        </div>
     );
 };
 
